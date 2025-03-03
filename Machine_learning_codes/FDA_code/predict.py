3894239e3ba7e487c9730e626b6dc27fa8b14d32
OMAR EL MAKKAOUI <makkaouio@gmail.com>
2025-03-04 00:18:38 +0100

    Delete Machine_learning_codes/FDA_code directory

--- Machine_learning_codes/FDA_code/predict.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def predict(X, projected_centroid, W):
-    
-    projected_data = np.dot(X, W)
-    distances = np.linalg.norm(projected_data[:, np.newaxis, :] - projected_centroid, axis=2)
-    label = np.argmin(distances, axis=1)
-    
-    return label
